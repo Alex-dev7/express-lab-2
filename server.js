e97e2c648eae9d09d27c63e5f5344cde93edee65
Alexei Rusu <rusualexei1995@gmail.com>
2022-11-04 10:36:42 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -4,9 +4,15 @@ const app = express()
 
 const PORT = 3000;
 
-
+//Greeting express application
 app.get('/greeting/:name', (request, response) => {
     response.send(`What's up, ${request.params.name} !`)
+})
+
+//Tip Calculator express application
+app.get('/tip/:total/:tipPercentage', (request, response) => {
+    let tip = parseInt(request.params.total) *( parseInt(request.params.tipPercentage) / 100)
+    response.send(`${tip}`)
 })
 
 app.listen(PORT, () => {
