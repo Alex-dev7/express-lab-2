cfe2b37f39a4a1f1f493e281dc72cca9f8b1f31b
Alexei Rusu <rusualexei1995@gmail.com>
2022-11-06 17:44:52 -0500

    pass it around app

---
+++ pass-it-around/server.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+
+const PORT = process.env.PORT 
+
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello</h1>')
+
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
